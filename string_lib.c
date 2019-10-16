d6e4a7964a4073c208be5bb65b2add5f472fe65d
mnovosad1095 <m.novosad@ucu.edu.ua>
2019-10-16 15:10:09 +0300

    create_string

--- string_lib.c
+++ string_lib.c
@@ -1,3 +1,35 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	size_t capacity_m; // Розмір блока
+	size_t size_m;	   // Фактичний розмір стрічки
+	char*  data;	   // Вказівник на блок пам'яті
+} my_str_t;
+
+
+int my_str_create(my_str_t* str, size_t buf_size);
+
+
 int main(int argc, char* argv[]){
+    my_str_t name;
+
+    my_str_create(&name, 3);
+
     return 0;
 }
+
+
+int my_str_create(my_str_t* str, size_t buf_size){
+    str -> size_m = buf_size;
+    str -> capacity_m = buf_size * 2;
+    str -> data = (char*) malloc(str -> capacity_m);
+    printf("%u\n", str -> data);    
+    return 0;
+}
+
+
+
+
